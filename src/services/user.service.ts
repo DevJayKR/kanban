b14b5767e79decf2ff01e80f3b2c10910f139915
Junsung Choi <choiexe1@gmail.com>
2023-11-20 22:39:11 +0900

    feat: serializer 사용 레이어를 컨트롤러 레이어로 변경

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,9 +1,7 @@
 import { Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
-import { User } from 'src/entities/user.entity';
 import { EncryptionHelper } from 'src/helpers/encryption.helper';
 import { PrismaService } from 'src/services/prisma.service';
-import { serializer } from 'src/utils/serializer';
 
 @Injectable()
 export class UserService {
@@ -21,15 +19,12 @@ export class UserService {
 			throw new UnprocessableEntityException('이미 존재하는 유저네임입니다.');
 		}
 
-		return serializer(
-			User,
-			await this.prisma.user.create({
-				data: {
-					username,
-					password: await this.encryptionHelper.encryption(password),
-				},
-			}),
-		);
+		return await this.prisma.user.create({
+			data: {
+				username,
+				password: await this.encryptionHelper.encryption(password),
+			},
+		});
 	}
 
 	async findOne(where: Prisma.UserWhereUniqueInput, select?: Prisma.UserSelect) {
@@ -46,7 +41,7 @@ export class UserService {
 		const matched = await this.encryptionHelper.compare(password, user.password);
 
 		if (matched) {
-			return serializer(User, user);
+			return user;
 		}
 	}
 }
